954984c738cb6e6143dd320c195ec5a57ba8a73b
Will Norris <willnorris@google.com>
2013-09-06 12:04:59 -0700

    restructure tests
    split tests for field types and field tags

--- query/encode_test.go
+++ query/encode_test.go
@@ -7,29 +7,46 @@ import (
 	"testing"
 )
 
-func TestValues(t *testing.T) {
+func TestValues_types(t *testing.T) {
 	s := struct {
-		A string `url:"a"`
+		A string
 		B int
-		C string
-		D string `url:"-"`
-		E string `url:",omitempty"`
-		F string `url:",omitempty"`
-	}{
-		A: "abc",
-		B: 1,
-		F: "foo",
-	}
+		C uint
+		D float32
+		E bool
+	}{}
 	v, err := Values(s)
 	if err != nil {
 		t.Errorf("Values(%q) returned error: %v", s, err)
 	}
 
 	want := url.Values{
-		"a": {"abc"},
-		"B": {"1"},
-		"C": {""},
-		"F": {"foo"},
+		"A": {""},
+		"B": {"0"},
+		"C": {"0"},
+		"D": {"0"},
+		"E": {"false"},
+	}
+	if !reflect.DeepEqual(want, v) {
+		t.Errorf("Values(%q) returned %v, want %v", s, v, want)
+	}
+}
+
+func TestValues_tags(t *testing.T) {
+	s := struct {
+		A string
+		B string `url:"so,omitempty"`
+		C string `url:"-"`
+		D string `url:"omitempty"` // actually named omitempty, not an option
+	}{}
+	v, err := Values(s)
+	if err != nil {
+		t.Errorf("Values(%q) returned error: %v", s, err)
+	}
+
+	want := url.Values{
+		"A":         {""},
+		"omitempty": {""},
 	}
 	if !reflect.DeepEqual(want, v) {
 		t.Errorf("Values(%q) returned %v, want %v", s, v, want)
