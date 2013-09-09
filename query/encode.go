ab27bc2c541fddbad0a4058eb1ab6d955c594609
Will Norris <willnorris@google.com>
2013-09-09 11:04:03 -0700

    add LICENSE (BSD)

--- query/encode.go
+++ query/encode.go
@@ -1,3 +1,7 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package query implements encoding of structs into URL query parameters.
 //
 // As a simple example:
